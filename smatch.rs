62e9c0685c3412ee63d083c12ee0bc97286feeff
George Zakhour <geezee@users.noreply.github.com>
2024-06-09 16:58:42 +0200

    Add license

--- smatch.rs
+++ smatch.rs
@@ -1,3 +1,22 @@
+/*
+smatch -- regexes for trees; grep for s-exprs
+
+Copyright (C) 2024  George Zakhour
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 use std::fmt::{Debug, Formatter};
 use std::rc::{Rc};
 use std::fs::{read_to_string, metadata, read_dir};
